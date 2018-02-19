c03af9a3391ba20bf4cbaf0964dd2aa1b70a39f1
Jeremy <jeremy@localhost.localdomain>
2018-02-19 16:40:24 +1100

    Adding bash_prompt, bash_profile, bashrc and updating bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-# Execute Settings
-sh .settings
+# Copy dotfiles
+cd "$(dirname "${BASH_SOURCE}")";
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude "bootstrap.sh" \
+		--exclude "applicationSettings/" \
+		--exclude ".settings" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+read -p "This may overwrite settings and existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+
+    # Execute Settings
+    sh .settings
+fi;
+
+unset doIt;
